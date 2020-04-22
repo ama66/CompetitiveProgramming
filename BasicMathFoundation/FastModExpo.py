05a52c6ecd2adfefc048de2dfd8f81b36e709d8a
Ammar Abdilghanie <ama66@cornell.edu>
2020-04-22 13:49:26 -0500

    Update FastModExpo.py

--- BasicMathFoundation/FastModExpo.py
+++ BasicMathFoundation/FastModExpo.py
@@ -1,3 +1,5 @@
+## Prime Factorization 
+import math
 #Fast Modular Exponentiation  
 ## A^2 mod C = (A * A) mod C = ((A mod C) * (A mod C)) mod C
 ## also (A^2)^ n mod c = (A^2 mod c)^ n  mod c 
@@ -7,8 +9,15 @@ n=2
 mod=10
 
 print("regular expo:", pow(a, n) % mod )
-
-def fast_exp(a,n,mod):
+def fast_exp_rec(a,n,mod):
+    if n==0:
+        return 1 
+    if n % 2 == 0 :
+        return fast_exp_rec((a*a) % mod , n/2 , mod)
+    return a % mod *fast_exp_rec(a , n-1 , mod) % mod 
+        
+    
+def fast_exp_it(a,n,mod):
     ans=1
     while n>=1:
         if (n%2==0):
@@ -21,9 +30,11 @@ def fast_exp(a,n,mod):
             print("Odd" , a , n , ans)
 
     return ans 
+print("Recursive ", fast_exp_rec(a,n,mod))
+print("Itertive ", fast_exp_it(a,n,mod))
 
-print("Itertive ", fast_exp(a,n,mod))
 regular expo: 6
+Recursive  6
 Even 6 1.0 1
 Odd 6 0.0 6
 Itertive  6
