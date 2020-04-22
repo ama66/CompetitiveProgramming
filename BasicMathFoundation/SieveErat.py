cad3b52186531e39f19fbf2d10be9619ea503575
Ammar Abdilghanie <ama66@cornell.edu>
2020-04-21 22:55:28 -0500

    Update SieveErat.py

--- BasicMathFoundation/SieveErat.py
+++ BasicMathFoundation/SieveErat.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def Sieve_Erat(N):
-    ## initialize array with True (all are prime unless we found some primes!)
+    ## initialize array with True (all are prime unless we invalidate this later!)
     for i in range(2,N+1):
         IsPrime[i]=1
     
