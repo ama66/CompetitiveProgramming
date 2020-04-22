f6bad0eb159e811398082cf59848a68d17fc04c3
Ammar Abdilghanie <ama66@cornell.edu>
2020-04-21 22:48:35 -0500

    Sieve of Eratosthenes optimized

---
+++ BasicMathFoundation/SieveErat.py
@@ -0,0 +1,24 @@
+# Sieve of Eratosthenes optimized
+import numpy as np
+
+
+def Sieve_Erat(N):
+    ## initialize array with True (all are prime unless we found some primes!)
+    for i in range(2,N+1):
+        IsPrime[i]=1
+    
+    for i in range(2,int((N+1)/2)):
+        if is_prime(i):
+            for j in range(2*i, N+1, i ):
+                 IsPrime[j]=0
+        
+
+N=20
+IsPrime=np.zeros(N+1)
+Sieve_Erat(N)
+
+for i in range(N+1):
+    if IsPrime[i]:
+        print(i)
+        
+2 3 5 7 11 13 17 19
