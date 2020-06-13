5bf510668ea7d49bdd5abc4c388dd262ddca705d
Ammar Abdilghanie <ama66@cornell.edu>
2020-06-12 21:56:47 -0500

    Update Valid_paranth.py

--- Stacks/Valid_paranth.py
+++ Stacks/Valid_paranth.py
@@ -1,1 +1,21 @@
 
+def match_par(c1,c2):
+    return c1,c2=="(",")" or c1,c2=="{","}" or c1,c2=="[","]"
+
+    
+
+def Is_Valid_Paran(s):
+    st=[]
+    for char in s:
+        if char in ["(","{","["]:
+            st.append(char)
+        else:
+            if len(st)!=0 and match_par(char,st[-1]):
+                st.pop()
+    
+    return len(st)==0
+            
+                
+Is_Valid_Paran("[{()}][")
+
+False
