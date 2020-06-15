82f6ca6ebb011891cbd477938b699e18301bd48d
Ammar Abdilghanie <ama66@cornell.edu>
2020-06-15 13:32:52 -0500

    Create KnapSackGreedy.py

---
+++ Greedy/KnapSackGreedy.py
@@ -0,0 +1,62 @@
+class KnapsackItem:
+    def __init__(self,w,v):
+        self.w=w
+        self.v=v
+        self.vpu=float(self.v)/float(self.w)
+    def __gt__(self, other):
+        if not isinstance(other, KnapsackItem):
+            return NotImplemented
+        return self.vpu > other.vpu
+    
+class KnapSack:
+    def __init__(self,G):
+        self.itemlist=[]
+        self.remaining_capacity=G
+        self.capacity=G
+        self.fill_bag=[]
+        self.n=0
+        
+    def Build_KnapSack(self,w_arr,v_arr):
+        for i in range(len(w_arr)):
+            self.itemlist.append(KnapsackItem(w_arr[i],v_arr[i]))
+        self.n=len(w_arr)
+        
+    def _Sort_By_Vpu(self):
+        self.itemlist=sorted(self.itemlist,reverse=True)
+    def Fill_KnapSack(self):
+        self._Sort_By_Vpu()
+        MaxCap=0
+        for i in range(self.n):
+            curr_weight=self.itemlist[i].w
+            curr_val=self.itemlist[i].v
+            ## check if I can insert it fully into the knapsack
+            if self.remaining_capacity - curr_weight >=0:
+                self.remaining_capacity-=curr_weight
+                MaxCap+=curr_val
+            else: # Check if I can take a fraction
+                frac=self.remaining_capacity/curr_weight
+                MaxCap+=curr_val*frac
+                self.remaining_capacity-=frac*curr_weight
+                break
+                
+        return MaxCap
+    
+        
+
+w=[2,3,5,7,1,4,1]
+v=[10,5,15,7,6,18,3]
+G=10
+
+# KS=KnapSack(1)
+# KS.Build_KnapSack(w,v)
+# KS.itemlist.sort(reverse=True)
+# [KS.itemlist[i].v for i in range(len(w))]
+# #KS.Fill_KnapSack()
+# [KS.itemlist[i].w for i in range(len(w))]
+#[KS.itemlist[i].w for i in range(len(w))]
+
+KS=KnapSack(10)
+KS.Build_KnapSack(w,v)
+KS.Fill_KnapSack()
+43.0
+
